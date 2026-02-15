bf0d83ab5eba6d4103adfb2ee490d1476e8ddc4d
aleksey <av.pavlow@gmail.com>
2026-02-15 20:53:39 +0300

    feat: Добавить основную часть алгоритма - Основная часть алгоритма добавлена

--- cmd/rate_limiter_token_bucket/rate_limiter_token_bucket.go
+++ cmd/rate_limiter_token_bucket/rate_limiter_token_bucket.go
@@ -7,28 +7,44 @@ import (
 )
 
 type TokenBucket struct {
-	tokens float64
-	capacity float64
+	tokens     float64
+	capacity   float64 //На случай если пришло одновременно больше токенов
 	lastUpdate time.Time
+	weight     float64
 
 	mu sync.Mutex
 }
 
-
-func Allow(tokenBucket TokenBucket) bool {
-
+func Allow(tokenBucket *TokenBucket) bool {
+	tokenBucket.mu.Lock()
 	defer tokenBucket.mu.Unlock()
+
+	now := time.Now()
+	diff := time.Since(tokenBucket.lastUpdate).Seconds()
+	tokenBucket.tokens += diff * tokenBucket.weight
+
+	if tokenBucket.tokens > tokenBucket.capacity {
+		tokenBucket.tokens = tokenBucket.capacity
+	}
+
+	tokenBucket.lastUpdate = now
+
+	if tokenBucket.tokens >= 1 {
+		tokenBucket.tokens--
+		return true
+	} 
+
 	return false
 }
 
 func main() {
-	tokenBucket := TokenBucket{tokens: 10, capacity: 10}
+	tokenBucket := TokenBucket{tokens: 10, capacity: 10, weight: 1, lastUpdate: time.Now()}
 
 	for i := range 100 {
-		if Allow(tokenBucket) {
-			fmt.Printf("Запрос %v разрешается делать", i + 1)
+		if Allow(&tokenBucket) {
+			fmt.Printf("Запрос %v разрешается делать", i+1)
 		} else {
-			fmt.Printf("Запрос %v не разрешается делать", i + 1)
+			fmt.Printf("Запрос %v не разрешается делать", i+1)
 		}
 	}
 }

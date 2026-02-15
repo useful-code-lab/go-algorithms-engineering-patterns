574744291a3dde44b4ffba848b828cbcf48bcac4
aleksey <av.pavlow@gmail.com>
2026-02-15 14:36:58 +0300

    feat: Добавить возможность отладки и доработать алгоритм - Возможность для отладки добавлена - Алгоритм в целом сделан

--- cmd/rate_limiter.go
+++ cmd/rate_limiter.go
@@ -3,18 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"golang.org/x/time/rate"
 )
 
 func main() {
-	limit := rate.Every(100 * time.Millisecond)
+
+	limit := rate.Limit(1000)
 	rateLimiter := rate.NewLimiter(limit, 2)
+	ctx := context.Background()
 
-	ctx = context.Background()
+	for i := 0; i <= 10; i++ {
+		err := rateLimiter.Wait(ctx)
 
-	for i:=0; i<=10; i++ {
+		if err != nil {
+			fmt.Printf("Ошибка: %v\n", err)
+			return
+		}
 
+		fmt.Printf("Печать %v\n", i)
 	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-algorithms
+
+go 1.24.0
+
+require golang.org/x/time v0.14.0

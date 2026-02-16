1b1ae17dc694577b24e1f811bad7e06864e9a853
aleksey <av.pavlow@gmail.com>
2026-02-16 20:49:31 +0300

    feat: Добавить полное решение задачи. WorkGroup добавлять не нужно - Реализовано полное решение задачи

--- cmd/worker_pool/worker_pool.go
+++ cmd/worker_pool/worker_pool.go
@@ -1,16 +1,31 @@
 package main
 
+import "fmt"
+
 func Worker(numWorker int, inp <-chan int, result chan<- int) {
-
+	for i := range inp {
+		result <- i * i
+	}
 }
-
 
 func main() {
 	digits := [10]int{1, 2, 4, 5, 6, 7, 8, 9, 10}
-	var result [10]int
 	chInput := make(chan int)
 	chOutput := make(chan int)
-	for i := range 10 {
+
+	for i := range 3 {
 		go Worker(i, chInput, chOutput)
+	}
+
+	go func() {
+		for i := range 10 {
+			chInput <- digits[i]
+		}
+		close(chInput)
+	}()
+
+	for range 10 {
+		r := <-chOutput
+		fmt.Printf("Вывод результата: %d\n", r)
 	}
 }

4bde2e3f6dea865431e2b1e200993099c6693598
aleksey <av.pavlow@gmail.com>
2026-02-16 14:27:38 +0300

    feat: Сделать корректное добавление оставльных элекиентов и вывод - Сделано корректное добавление и корректный вывод

--- cmd/inplace_reverse/inplace_reverse.go
+++ cmd/inplace_reverse/inplace_reverse.go
@@ -14,6 +14,12 @@ type LinkedList struct {
 func (list *LinkedList) Add(newNode *Node) {
 	if list.Head == nil {
 		list.Head = newNode
+	} else {
+		current := list.Head
+		for current.Next != nil {
+			current = current.Next
+		}
+		current.Next = newNode
 	}
 }
 
@@ -22,11 +28,19 @@ func main() {
 	node := Node{Value: 1}
 	list.Add(&node)
 	
-	current := list.Head
-	fmt.Printf("1-й элемент : %v",current.Value)
 
-	for current != nil {
-
+	for i := range(5) {
+		list.Add(&Node{Value: i+6})
 	}
+
+	current := list.Head
+	i := 1
+
+	for current.Next != nil {
+		fmt.Printf("%d-й элемент : %d\n", i, current.Value)
+		current = current.Next
+		i++
+	}
+	fmt.Printf("%d-й элемент : %d\n", i, current.Value)
 
 }

224e34200abe3e9a7730a6e59d346e7d26e8c2a4
aleksey <av.pavlow@gmail.com>
2026-02-16 13:59:15 +0300

    feat: Реализовать добавление 1-го элемента к списку - Добавление 1-го элемента к списку реализовано

--- cmd/inplace_reverse/inplace_reverse.go
+++ cmd/inplace_reverse/inplace_reverse.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Value int
 	Next *Node
@@ -9,12 +11,22 @@ type LinkedList struct {
 	Head *Node
 }
 
-func (*LinkedList) Add(newNode *Node) {
-
+func (list *LinkedList) Add(newNode *Node) {
+	if list.Head == nil {
+		list.Head = newNode
+	}
 }
 
 func main() {
 	list := &LinkedList{}
 	node := Node{Value: 1}
 	list.Add(&node)
+	
+	current := list.Head
+	fmt.Printf("1-й элемент : %v",current.Value)
+
+	for current != nil {
+
+	}
+
 }

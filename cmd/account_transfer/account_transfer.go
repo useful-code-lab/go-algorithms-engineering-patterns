7d822905ff81f7fa792590685415e3cb59f46cf9
aleksey <av.pavlow@gmail.com>
2026-02-17 15:25:29 +0300

    feat: Реализовать основную структуру программы переводов денег - Основная структура переводов денег реализована

--- cmd/account_transfer/account_transfer.go
+++ cmd/account_transfer/account_transfer.go
@@ -1,6 +1,20 @@
 package main
 
+import "fmt"
+
+type Account struct {
+	ID int
+	Balance int64
+}
+
+func Transfer(from *Account, to *Account) {
+
+}
 
 func main(){
-	
+	petr := &Account{ID: 1, Balance: 50000}
+	ivan := &Account{ID: 2, Balance: 10000}
+	fmt.Printf("До перевода: Пётр(%d) - %d, Иван(%d) - %d\n", petr.ID,  petr.Balance, ivan.ID, ivan.Balance)
+	Transfer(petr, ivan)
+	fmt.Printf("После первода: Пётр(%d) - %d, Иван(%d) - %d", petr.ID,  petr.Balance, ivan.ID, ivan.Balance)
 }

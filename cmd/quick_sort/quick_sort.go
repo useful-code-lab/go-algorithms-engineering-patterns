24060d42825e7c7db44128c187ccfe5ed0068dc1
aleksey <av.pavlow@gmail.com>
2026-02-17 13:33:42 +0300

    feat: Добавить основные элементы алгоритма для быстрой сортировки - Основные элементы алгоритма добавлены

--- cmd/quick_sort/quick_sort.go
+++ cmd/quick_sort/quick_sort.go
@@ -1,5 +1,41 @@
 package main
 
+import "fmt"
+
+func QuickSort(arr []int) {
+
+}
+
+func QuickSortRecursive(arr []int, low, high int){
+	if low >= high {
+		return
+	}
+
+	pivot := Partition(arr, low, high)
+	QuickSortRecursive(arr, low, pivot - 1)
+	QuickSortRecursive(arr, pivot + 1, high)
+}
+
+func Partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low - 1
+
+	for j := low; j <= high - 1; j++ {
+		if 	arr[j] <= pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i] 
+		}
+	}
+
+	arr[high], arr[i + 1] = arr[i + 1], arr[high]
+
+	return i + 1
+}
+
+
 func main(){
-	
+	arr := []int{3, 4, 7, 3, 7, 10, 9, 8}
+	fmt.Println(arr)
+
+	//QuickSort(arr)
 }

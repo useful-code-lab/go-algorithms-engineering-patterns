140c4f5df99147617d9a90b2c7e2b9d4d4c496e8
aleksey <av.pavlow@gmail.com>
2026-02-17 13:38:35 +0300

    feat: Добавить полную реализацию быстрой сортировки массивов с использованием опорных чисел - Полная реализация сортировки мссива добавлена

--- cmd/quick_sort/quick_sort.go
+++ cmd/quick_sort/quick_sort.go
@@ -3,39 +3,40 @@ package main
 import "fmt"
 
 func QuickSort(arr []int) {
-
+	QuickSortRecursive(arr, 0, len(arr)-1)
 }
 
-func QuickSortRecursive(arr []int, low, high int){
+func QuickSortRecursive(arr []int, low, high int) {
 	if low >= high {
 		return
 	}
 
 	pivot := Partition(arr, low, high)
-	QuickSortRecursive(arr, low, pivot - 1)
-	QuickSortRecursive(arr, pivot + 1, high)
+	QuickSortRecursive(arr, low, pivot-1)
+	QuickSortRecursive(arr, pivot+1, high)
 }
 
 func Partition(arr []int, low, high int) int {
 	pivot := arr[high]
 	i := low - 1
 
-	for j := low; j <= high - 1; j++ {
-		if 	arr[j] <= pivot {
+	for j := low; j <= high-1; j++ {
+		if arr[j] <= pivot {
 			i++
-			arr[i], arr[j] = arr[j], arr[i] 
+			arr[i], arr[j] = arr[j], arr[i]
 		}
 	}
 
-	arr[high], arr[i + 1] = arr[i + 1], arr[high]
+	arr[high], arr[i+1] = arr[i+1], arr[high]
 
 	return i + 1
 }
 
-
-func main(){
+func main() {
 	arr := []int{3, 4, 7, 3, 7, 10, 9, 8}
-	fmt.Println(arr)
+	fmt.Println("Исходный массив: ", arr)
 
-	//QuickSort(arr)
+	QuickSort(arr)
+
+	fmt.Println("Отсортированный массив: ", arr)
 }

050e5825f9f28392f55cfae17ecd8c4ba40dc504
aleksey <av.pavlow@gmail.com>
2026-02-17 15:38:23 +0300

    feat: Реализовать окончательную версию перевода денег - Перевод денег окончательно реализован

--- cmd/account_transfer/account_transfer.go
+++ cmd/account_transfer/account_transfer.go
@@ -1,20 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Account struct {
-	ID int
+	ID      int
 	Balance int64
 }
 
-func Transfer(from *Account, to *Account) {
+func Transfer(from *Account, to *Account, amount int64) error {
+	if amount < 0 {
+		return errors.New("Сумма перевода не может быть отрицательной.")
+	}
 
+	if from.Balance < amount {
+		return fmt.Errorf("Недостаточно денег для перевода на счёте %d", from.ID)
+	}
+
+	from.Balance -= amount
+	to.Balance += amount
+
+	return nil
 }
 
-func main(){
+func main() {
 	petr := &Account{ID: 1, Balance: 50000}
 	ivan := &Account{ID: 2, Balance: 10000}
-	fmt.Printf("До перевода: Пётр(%d) - %d, Иван(%d) - %d\n", petr.ID,  petr.Balance, ivan.ID, ivan.Balance)
-	Transfer(petr, ivan)
-	fmt.Printf("После первода: Пётр(%d) - %d, Иван(%d) - %d", petr.ID,  petr.Balance, ivan.ID, ivan.Balance)
+	fmt.Printf("До перевода: Пётр(%d) - %d, Иван(%d) - %d\n", petr.ID, petr.Balance, ivan.ID, ivan.Balance)
+	err := Transfer(petr, ivan, 5000)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("После перевода: Пётр(%d) - %d, Иван(%d) - %d\n", petr.ID, petr.Balance, ivan.ID, ivan.Balance)
+	}
+
+	err = Transfer(petr, ivan, -4000)
+	if err != nil {
+		fmt.Println(err)
+
+	} else {
+		fmt.Printf("После перевода: Пётр(%d) - %d, Иван(%d) - %d\n", petr.ID, petr.Balance, ivan.ID, ivan.Balance)
+	}
+
+	err = Transfer(petr, ivan, 774000)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("После перевода: Пётр(%d) - %d, Иван(%d) - %d\n", petr.ID, petr.Balance, ivan.ID, ivan.Balance)
+	}
+
 }

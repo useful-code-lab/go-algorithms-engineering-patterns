f7a8bb23b47e93f4ecd45dc90f71b44a8298f7b4
aleksey <av.pavlow@gmail.com>
2026-02-21 21:41:06 +0300

    feat: Добавить предварительную структуру сортировки - Предварительная структура сортировки добавлена

--- cmd/merge_sort/merge_sort.go
+++ cmd/merge_sort/merge_sort.go
@@ -1,7 +1,22 @@
 package main
 
+type MyNumbers interface {
+	~int | ~int64 | ~float64 | ~string
+}
 
+func MergeSort[T MyNumbers](a []T) []T {
+	
+	return merge()  
+}
 
-func main(){
+func merge[T MyNumbers](a []T, b []T) []T{
+	result := make([]T, len(a) + len(b))
+	return result
+}
+
+func main() {
+	arr := []int{4, 6, 7, 8, 9, 6}
+
+	MergeSort(arr)
 
 }

160469bbd61e86997d0f30a52844eef9298ab3bd
aleksey <av.pavlow@gmail.com>
2026-02-22 10:44:27 +0300

    feat: Добавить  алгоритм до конца - алгоритм добавлен до конца

--- cmd/bfs/bfs.go
+++ cmd/bfs/bfs.go
@@ -1,10 +1,24 @@
 package main
 
-func BFS(start string, graph map[string][]string) {
-	startNode = graph[start]
-	visited := map[string]bool{start: true,}
-	queue := []string{start,}
+import "fmt"
 
+func BFS(start string, graph map[string][]string) {
+	visited := map[string]bool{start: true}
+	queue := []string{start}
+
+	fmt.Println("Посещённые элементы:")
+	if len(queue) > 0 {
+		element := queue[0]
+		queue = queue[1:]
+
+		for _, el := range graph[element] {
+			if !visited[el] {
+				visited[el] = true
+				queue = append(queue, el)
+				fmt.Printf("%s ", el)
+			}
+		}
+	}
 }
 
 func main() {

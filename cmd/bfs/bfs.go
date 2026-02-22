e00057fbfc3759be85438aeb535f75173043bbb3
aleksey <av.pavlow@gmail.com>
2026-02-22 09:51:28 +0300

    feat: Добавить предварительную структуру алгоритма - Предварительная структура алгоритма добавлена

--- cmd/bfs/bfs.go
+++ cmd/bfs/bfs.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-	
+func BFS(graph map[string][]string) {
+
+}
+
+func main() {
+	Graph := map[string][]string{
+		"A": {"B", "C"},
+		"B": {"D", "F"},
+		"C": {"K", "L"},
+	}
+	BFS(Graph)
 }

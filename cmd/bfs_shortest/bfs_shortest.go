33e2e3528cca4c63e9a8ed144717d626b8492789
aleksey <av.pavlow@gmail.com>
2026-02-22 13:39:39 +0300

    feat: Реализовать основную часть алгоритма - Основная часть алгоритма реализована

--- cmd/bfs_shortest/bfs_shortest.go
+++ cmd/bfs_shortest/bfs_shortest.go
@@ -1,5 +1,45 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func BFS(gpaph map[string][]string, start string, goal string) []string {
+	visited := map[string]bool{start: true}
+	queue := []string{start}
+	parent := map[string]string{start: ""}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[:1]
+
+
+		if current == goal {
+
+		}
+
+		for _, child := range gpaph[current]{
+			if !visited[child]{
+				parent[child] = current
+				visited[child] = true
+				queue = append(queue, child)
+			}
+		}
+	}
+
+	return nil
+}
+
+func collectPath(parent map[string]string, goal string) []string {
+	return nil
+}
+
+func main() {
+	graph := map[string][]string{
+		"A": {"B", "C"},
+		"B": {"D", "F"},
+		"D": {"K", "L"},
+		"F": {"S", "T"},
+	}
+
+	result := BFS(graph, "A", "L")
+	fmt.Print(result)
 }
